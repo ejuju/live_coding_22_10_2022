eed3d2a8c4cc6dc02c298abc932e8d10fa1b39f7
ejuju <juju.sel.mail@gmail.com>
2022-10-23 19:45:59 +0200

    last modifications

--- src/lib/herd.ts
+++ src/lib/herd.ts
@@ -1,7 +1,7 @@
-import { Vector } from "p5";
+import { Color, Vector } from "p5";
 import type p5 from "p5";
 
-const MAX_STEERING_FORCE = 0.3;
+const MAX_STEERING_FORCE = 0.2;
 
 export class Herd {
     private _: p5;
@@ -11,8 +11,8 @@ export class Herd {
     // Territory determines where people are born.
     // Black hole kills some individuals as time passes.
     private individuals: Individual[] = [];
-    private birthsPerTick: number = 0;
-    private birthSurvivalRate: number = 0.01;
+    private birthsPerTick: number = 1;
+    private birthSurvivalRate: number = 0.1;
     private tickSurvivalRate: number = 1;
     private territory: Vector;
     private blackHole: BlackHole;
@@ -52,7 +52,7 @@ export class Herd {
     // Render herd inside simulation.
     render() {
         this.individuals.forEach(i => i.render());
-        this.blackHole.render();
+        // this.blackHole.render();
     }
 }
 
@@ -66,6 +66,8 @@ export class Individual {
     private velocity: Vector;
     private acceleration: Vector;
     private maxSpeed: number;
+    private size: number;
+    private color: Color;
 
     // Alignment: Do you want to go where others are going?
     // Cohesion: Do you want to stay within the herd?
@@ -83,15 +85,27 @@ export class Individual {
         this.velocity = Vector.random2D();
         this.velocity.setMag(_.random(1, 2));
         this.acceleration = _.createVector();
-        this.maxSpeed = 3;
+        this.maxSpeed = 4;
+        this.size = 0.5;
 
-        this.seperationBias = _.random(1, 2); // 1, 2
-        this.alignmentBias = _.random(0, 1); // 0.5, 1.5
-        this.cohesionBias = _.random(0, 0); // 0.1, 1
+        this.seperationBias = _.random(1.5, 2); // 1, 2
+        this.alignmentBias = _.random(1, 2); // 0.5, 1.5
+        this.cohesionBias = _.random(0, 2); // 0.1, 1
+
+        this.color = _.color(
+            this.seperationBias * 120,
+            this.alignmentBias * 120,
+            this.cohesionBias * 120,
+        )
     }
 
     // Render this individual inside simulation.
-    render() { this._.circle(this.position.x, this.position.y, 3) }
+    render() {
+        this._.push();
+        this._.fill(this.color)
+        this._.circle(this.position.x, this.position.y, this.size)
+        this._.pop();
+    }
 
     // Move forward in time.
     tick(others: Individual[], blackhole: BlackHole) {
@@ -140,7 +154,7 @@ export class Individual {
 
     // Calculates where to go to align with others.
     private align(others: Individual[]): Vector {
-        const depth = 70;
+        const depth = 40;
         const steering = this._.createVector();
         let total = 0;
         others.forEach(o => {
@@ -212,10 +226,12 @@ export class Individual {
 export class BlackHole {
     private _: p5;
     position: Vector;
+    private size: number;
 
     constructor(_: p5, position: Vector) {
         this._ = _;
         this.position = position;
+        this.size = 20;
     }
 
     // Calculates the force to apply according to the position of an object.
@@ -224,7 +240,22 @@ export class BlackHole {
     }
 
     render() {
-        this._.random(-1)
-        this._.circle(this.position.x, this.position.y, 10)
+        this._.push();
+        this._.noFill();
+        this._.strokeWeight(1);
+        // this._.bezier(
+        //     this.position.x,
+        //     this.position.y,
+        //     this.position.x + this._.randomGaussian(-this.size * 2, this.size * 2),
+        //     this.position.y + this._.randomGaussian(-this.size * 2, this.size * 2),
+        //     this.position.x + this._.randomGaussian(-this.size * 2, this.size * 2),
+        //     this.position.y + this._.randomGaussian(-this.size * 2, this.size * 2),
+        //     this.position.x,
+        //     this.position.y
+        // )
+
+        this._.fill("#ffffff05")
+        this._.circle(this.position.x, this.position.y, this.size)
+        this._.pop();
     }
 }
--- src/lib/sketch_0.ts
+++ src/lib/sketch_0.ts
@@ -1,8 +1,6 @@
-import type { Renderer } from "p5";
 import type p5 from "p5";
 import { BlackHole, Herd, Individual } from "./herd";
 import { calcHeight, calcWidth } from "./utils";
-
 
 const INITIAL_POPULATION = 100;
 
@@ -11,14 +9,16 @@ export default (_: p5) => {
     const height = calcHeight()
     const blackhole = new BlackHole(_, _.createVector(width / 2, height / 2))
     const herd = new Herd(_, _.createVector(width, height), blackhole)
+    const background = "#000000";
+    const backgroundContrast = "#ffffff";
 
     _.setup = () => {
         _.createCanvas(width, height);
-        _.background("#fff");
-        _.fill("#000");
-        _.stroke("#000");
+        _.background(background);
+        _.fill(backgroundContrast);
+        _.stroke(backgroundContrast);
         _.strokeWeight(0.5);
-        _.textSize(16);
+        _.textSize(40);
 
         for (let i = 0; i < INITIAL_POPULATION; i++) {
             herd.add(new Individual(
@@ -30,25 +30,19 @@ export default (_: p5) => {
     }
 
     _.draw = () => {
-        _.background("#ffffff44");
+        // _.background(background + "44");
         const text_lines = [
-            "Instinct de séparation",
-            "Instinct d'alignement",
-            "Instinct de cohésion",
+            "Séparation - Rouge",
+            "Alignement - Vert",
+            "Cohésion - Bleu",
         ];
-        text_lines.forEach((txt, i) => { _.text(txt, 50, (i + 1) * 50) })
+        text_lines.forEach((txt, i) => { _.text(txt, 50, (i + 1) * 75) })
 
-        if (_.mouseX < 100 ||
-            _.mouseX > _.width - 100 ||
-            _.mouseY < 100 ||
-            _.mouseY > _.height - 100
-        ) {
-            blackhole.position.x = _.width / 2;
-            blackhole.position.y = _.height / 2;
-        } else {
-            blackhole.position.x = _.mouseX;
-            blackhole.position.y = _.mouseY;
-        }
+        // if (_.mouseX > 100 && _.mouseX < _.width - 100 &&
+        //     _.mouseY > 100 && _.mouseY < _.height - 100) {
+        //     blackhole.position.x = _.mouseX;
+        //     blackhole.position.y = _.mouseY;
+        // }
 
         herd.tick()
         herd.render()
